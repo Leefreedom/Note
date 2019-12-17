510add0852e4e84f98529a7b3fac675bf3749057
hy_lxh <292290747@qq.com>
2019-12-17 17:45:48 +0800

    vue-router

---
+++ note/vue/vueRouter/vue.js
@@ -0,0 +1,20 @@
+/*
+    *********************   vue-router    *********************
+*/
+
+//安装：npm install vue-router
+
+//在vue的入口文件中引入，并挂载到根实例中去。
+
+
+//在单页面中可以使用router-link 组件来导航，<router-link> 默认会被渲染成一个 `<a>` 标签
+//  <router-link to="/next">Router</router-link>//导航到next页面
+
+/* <router-view></router-view> //路由匹配到的组件将渲染在这里 */
+
+
+/* 动态路由匹配 */
+
+
+
+
