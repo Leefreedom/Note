5bded382dcaeb7ff4100de06f0526011dcb47a7b
lxh <50310416+Leefreedom@users.noreply.github.com>
2020-01-22 00:33:38 +0800

    选择器，css特性

--- note/html+css/8.字体样式.js
+++ note/html+css/8.字体样式.js
@@ -2,6 +2,22 @@
     字体样式
       1、字体大小：font-size
       2、字体类型：font-family
-      3、字体粗度：font-weight
+      3、字体粗度：font-weight（400-normal不加粗，700-bold加粗）
       4、字体风格：font-style
+
+    文本样式
+      5、文本颜色：color
+      6、段落行间距：line-height
+      7、文本水平对齐方式：text-align（left，right，center）
+      8、段落首行缩进：text-indent（px,em字符,）
+      9、文本下划线underline  上划线overline  删除线line-through：text-decoration：none无
+*/
+
+
+/*
+    文本格式化标签
+        1、em和i标签：font-style：normal正常  italic倾斜
+        2、strong和b标签：font-weight：normal正常  bold加粗
+        3、u和ins标签：text-decoration：none无  underline下划线
+        4、s和del标签：text-decoration：none无  line-through下划线
 */
